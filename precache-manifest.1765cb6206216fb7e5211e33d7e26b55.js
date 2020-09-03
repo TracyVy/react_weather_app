747ed802182bf01887ad9326664278b1b522b165
Tracy Vy <tracyvy88@gmail.com>
2020-09-03 10:29:21 -0700

    Updates

---
+++ precache-manifest.1765cb6206216fb7e5211e33d7e26b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d2d18358f0ff92afb47557528bf35b",
+    "url": "/react_weather_app/index.html"
+  },
+  {
+    "revision": "5a45bfc1aa434cdd1056",
+    "url": "/react_weather_app/static/css/main.7692b378.chunk.css"
+  },
+  {
+    "revision": "4c3b27f189975e3ff6c1",
+    "url": "/react_weather_app/static/js/2.34f27ae8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_weather_app/static/js/2.34f27ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a45bfc1aa434cdd1056",
+    "url": "/react_weather_app/static/js/main.9bfa3503.chunk.js"
+  },
+  {
+    "revision": "f898914bdeb918ce1ba6",
+    "url": "/react_weather_app/static/js/runtime-main.970bf947.js"
+  }
+]);
